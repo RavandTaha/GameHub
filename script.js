7ebf47525b1c2c32f205a877a4d64632305d59f7
Ravand <ravand01@hotmail.com>
2023-01-08 10:17:08 +0100

    updated css, js and html

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const bar = document.getElementById('bar');
-const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
 
 if (bar) {
   bar.addEventListener('click',() =>{
